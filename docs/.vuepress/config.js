173f23b609fe4d0a9cadce6c349220630996fb25
common0sky <shenxinle2009@163.com>
2019-09-30 17:12:37 +0800

    add nav && sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,34 @@
 module.exports = {
+  base: '/tombstone/',
   title: 'tombstone',
-  description: 'docs'
+  description: 'tombstone docs',
+  head: [],
+  port: 3009,
+  themeConfig: {
+    nav: [
+      {
+        text: 'about',
+        link: '/about'
+      }
+    ],
+
+    sidebarDepth: 1, // 默认 1， 提取到 h2 (h1 是作为 sidebar 标题的)
+    activeHeaderLinks: false, // 禁用滚动时 url hash 更新
+    sidebar: [
+      ['/', 'home'],
+      {
+        title: 'handbook',
+        path: '/handbook/',
+        collapsable: true, // 默认 true, 可折叠； 设为 false 永远展开
+        sidebarDepth: 1,
+        children: [
+          '/handbook/git'
+        ]
+      },
+      {
+        title: 'blog',
+        path: '/blog/',
+      }
+    ]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,19 @@
 # Hello 
+
+## h21
+
+### h3
+
+<div style="height: 500px" ></div>
+
+## h22
+
+### h3
+
+<div style="height: 500px" ></div>
+
+## h23
+
+### h3
+
+<div style="height: 500px" ></div>
